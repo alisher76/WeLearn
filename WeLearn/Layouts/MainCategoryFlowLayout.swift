333b3801cbb267797619d2e6e6d3d489c3185900
AliGorithm <alisher.coder@gmail.com>
2018-03-27 16:35:32 -0500

    MainPageCellSnapFeatureAdded

---
+++ WeLearn/Layouts/MainCategoryFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  MainCategoryFlowLayout.swift
+//  WeLearn
+//
+//  Created by Alisher Abdukarimov on 3/27/18.
+//  Copyright © 2018 Alisher Abdukarimov. All rights reserved.
+//
+
+import UIKit
+
+class MainCategoryFlowLayout: UICollectionViewFlowLayout {
+
+}
